cb374fe99b0e9db58f60eacfc73b76198d793966
Diego Versiani <diegoversiani@gmail.com>
2017-07-14 08:17:52 +0200

    Move default editor to simple template options.

--- inc/shortcodes/content-block/register-template-simple.php
+++ inc/shortcodes/content-block/register-template-simple.php
@@ -6,5 +6,43 @@ function underskeleton_ctb_register_content_block_template_simple() {
       'name'               => 'simple',
       'label'              => __('Simple', 'underskeleton_ctb' ),
     ) );
+
+  add_action( 'underskeleton_ctb_options_metabox', 'underskeleton_ctb_template_simple_options_metabox' );
+  
+  add_filter( 'underskeleton_ctb_before_save_block_options', 'underskeleton_ctb_template_simple_before_save' );
 }
 add_action( 'init', 'underskeleton_ctb_register_content_block_template_simple' );
+
+
+
+function underskeleton_ctb_template_simple_options_metabox( $block_options ) {
+  global $post;
+  $content = $post->post_content;
+
+  // create nonce
+  wp_nonce_field( basename( __FILE__ ), 'underskeleton_ctb_template_simple_options_nonce' );
+  ?>
+
+  <div class="content-block-template-options content-block-simple-options">
+    <p>
+      <?php wp_editor( $content, 'content' ); ?>
+    </p>
+  </div>
+
+  <?php
+}
+
+
+
+
+
+function underskeleton_ctb_template_simple_before_save( $block_options ) {
+
+  if ( wp_verify_nonce( $_POST['underskeleton_ctb_template_simple_options_nonce'], basename( __FILE__ ) )
+    && $block_options['template'] == 'content-block-simple' ) {
+    global $post;
+    $post->post_content = wp_kses_post( $_POST['content'] );
+  }
+
+  return $block_options;
+}
